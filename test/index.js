f336858882f334aaac9c2d551d2645019c78a79a
gamegee <simon.bruneaud@gmail.com>
2018-06-26 14:12:27 +0200

    remove test root dir

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import { assert } from 'chai';
-import awesomeFunction from '../src/awesomeModule';
-
-describe('Awesome test.', () => {
-  it('should test awesome function', () => {
-    assert(awesomeFunction() === true, 'Not awesome :(');
-  });
-});
